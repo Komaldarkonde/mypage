f60287aa18e179baca9df74b56babdf096b58df3
Komaldarkonde <106942290+Komaldarkonde@users.noreply.github.com>
2024-11-27 22:00:00 -0800

    Add files via upload

---
+++ string.py
@@ -0,0 +1,59 @@
+text = "Learing Python is fun and educational!"
+
+print("Original text:", text)
+
+print("Sliced text (first 8 characters):", text[:8])
+
+print("Sliced text (last 4 characters):", text[-4])
+
+print("Sliced text (index 9 to 15):", text[9:16])
+
+print("Sliced text (every 2nd character):", text[::2])
+
+print("sliced text (reversed):", text[::-1])
+
+number = 1234
+number_str = str(number)
+print("Number as a string:", number_str)
+
+pi = 3.14159
+pi_str = str(pi)
+print("Pi as a string:", pi_str)
+
+number_str2 = "5678"
+number_int = int(number_str2)
+print("String '5678'as an integer:", number_int)
+
+float_str = "12.34"
+float_num = float(float_str)
+print("String '12.34' as a float:", float_num)
+
+number = 1234
+number_str = str(number)
+print("Number as a string:", number_str)
+
+pi = 3.14159
+pi_str = str(pi)
+print("Pi as a string:", pi_str)
+
+number_str2 = "5678"
+number_int =(number_str2)
+print("String '5678' as an integer:", number_int)
+
+float_str = "12.34"
+float_num = float(float_str)
+print("String '12.34' as a float:", float_num)
+
+pi_int = int(pi)
+print("Float 3.14159 as an integer:", pi_int)
+
+number_float = float(number)
+print("Integer 1234 as a float:", number_float)
+
+print("Uppercase text:", text.upper())
+print("Lowercase text:", text.lower())
+print("Replacing 'fun' with 'exciting':", text.replace("fun", "exciting"))
+print("Is the text alphanumeric?", text.isalnum())
+print("Is the text alphabetic?", text.isalpha())
+
+print("Length of the text:", len(text))
